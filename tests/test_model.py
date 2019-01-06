fa7309ecb991be8b3c1e7866f37c226c5543855b
mohashi <mohashi@gmail.com>
2019-01-06 20:31:32 -0200

    Fixing unit tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -20,23 +20,25 @@ class TestClient(AsyncTestCase):
         self.assertEqual(clients[1], client2, "It should be equal!")
 
     @gen_test
-    def test_delete(self):
+    def test_remove(self):
         client1 = Client("1", "Pai", "pai@test.com", date.today())
         client2 = Client("2", "Mae", "mae@test.com", date.today())
-        saved_client1 = yield client1.save()
-        saved_client2 = yield client2.save()
+        yield client1.save()
+        yield client2.save()
 
         clients = yield Client.find(lambda c: True)
         clients = list(clients)
         self.assertEqual(2, len(clients), "It should have 2 values!")
 
-        deleted_client = yield client1.remove()
+        deleted_client1 = yield client1.remove()
+        self.assertEqual(client1, deleted_client1[0], "It show be equal!")
         clients = yield Client.find(lambda c: True)
         clients = list(clients)
         self.assertEqual(1, len(clients), "It should have one values!")
         self.assertEqual(clients[0], client2, "It should be equal!")
 
-        deleted_client = yield client2.remove()
+        deleted_client2 = yield client2.remove()
+        self.assertEqual(client2, deleted_client2[0], "It show be equal!")
         clients = yield Client.find(lambda c: True)
         clients = list(clients)
         self.assertEqual(0, len(clients), "It should have zero values!")
@@ -45,8 +47,8 @@ class TestClient(AsyncTestCase):
     def test_find(self):
         client1 = Client("1", "Pai", "pai@test.com", date.today())
         client2 = Client("2", "Mae", "mae@test.com", date.today())
-        saved_client1 = yield client1.save()
-        saved_client2 = yield client2.save()
+        yield client1.save()
+        yield client2.save()
 
         clients = yield Client.find(lambda c: c.name == "Pai")
         clients = list(clients)
