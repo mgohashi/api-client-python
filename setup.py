cc4ab7a0676b2c63ea39d52756021db8db1ffe22
mohashi <mohashi@gmail.com>
2019-01-05 23:23:31 -0200

    Fixing setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 
+requires = [
+    "tornado"
+]
+
 setup(
     name='ApiClient',
     version='0.1',
@@ -9,6 +13,7 @@ setup(
     author='Marcelo Ohashi',
     author_email='mohashi@gmail.com',
     description='API Client with some methods to demonstrate a REST API',
+    install_reqs=requires,
     entry_points='''
         [console_scripts]
         run-api-client=apiclient:main
